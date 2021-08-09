b12ae68cebadd26cd20631aed76ee27ac8f3ed25
Alejandro <alejandro.ordenes.o@gmail.com>
2021-08-09 14:47:42 -0400

    commit angular basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contadores/contador.module';
+
+
+//Crear module llamado contador 
+// declaraciones y exportar para usar component contador
+
 @NgModule({
   declarations: [
     AppComponent
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
